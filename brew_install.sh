2c767923474b1fa65211341f7361b7f1ce4d35fe
Hector R <birkoff.h@gmail.com>
2018-10-02 16:23:05 +0200

    brew install
    brew install

---
+++ brew_install.sh
@@ -0,0 +1,8 @@
+# Install HomeBrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install all packages
+brew install wget
+brew install homebrew/cask/alfr
+
+brew cask install google-chrome iterm2 pycharm-ce slack github visual-studio-code iterm2
